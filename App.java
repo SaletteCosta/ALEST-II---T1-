77a2bb983040e64c8641ec541e96ae6038ba5dc6
Salette <kimsalette@gmail.com>
2024-09-15 17:52:24 -0300

    inico app

--- App.java
+++ App.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class App {
+    private Scanner entrada;
+    public App() {
+            try {
+                BufferedReader streamEntrada = new BufferedReader(new FileReader("casof30"));
+                entrada = new Scanner(streamEntrada);
+            } catch (FileNotFoundException e) {
+               e.printStackTrace();
+            }
+    }
 }
