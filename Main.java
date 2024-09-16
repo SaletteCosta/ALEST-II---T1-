3f01fe685468ca3d9f67adbc4ecd3e53c98ecb37
Salette <kimsalette@gmail.com>
2024-09-16 00:08:50 -0300

    Metodo achar raiz que dara inicio ao metodo percorrer arvore

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         LeituraArvore la = new LeituraArvore();
+        ManipulaMatriz mr = new ManipulaMatriz();
         char[][] matrizArvore = la.lerArquivo("casof30.txt");
         for (int i = 0; i < matrizArvore.length; i++) {
             System.out.println(matrizArvore[i]);
-        }
     }
+        System.out.println(mr.acharRaiz());
+  }
 }
--- ManipulaMatriz.java
+++ ManipulaMatriz.java
@@ -1,20 +1,35 @@
 public class ManipulaMatriz {
     private char matriz [][];
+    public ManipulaMatriz(){
+        pegarMatriz();
+    }
 
     private void pegarMatriz(){
-        matriz = LeituraArvore.lerArquivo("casof30");
+        matriz = LeituraArvore.lerArquivo("casof30.txt");
     }
 
     private int encontrarMaiorCaminho(){
         return 0;
     }
-    public void percorrerArvore(){
 
+    public int percorrerArvore(int linha, int coluna){
+        acharRaiz();
+        return linha;
     }
 
+    public char acharRaiz() {
+        int ultimaLinha = LeituraArvore.getUltimaLinha();
+        for (int i = 0; i < matriz[ultimaLinha].length; i++) {
+            if(matriz[ultimaLinha][i] != ' '){
+            return matriz[ultimaLinha][i];
+        }
+    }
 
     // iremos trabalhar com manipulação de indices da matriz ex: o filho a esquerda esta uma linha acima e uma coluna a frente
     // ou seja se o pai esta na linha 23 coluna 20, o filho esta na linha 22 coluna 19
     //o filho a direita dp pai na linha 23, cpluna 20 esta na linha 22 coluna 21
     // quando houver cruzamento, sobe a linha e verifica o char a esquerda pu dieita para ver s ha um outro traço, numero ou nodo folha
+
+        return 0;
+    }
 }
